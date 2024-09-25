432fdaf1c30201d91170f76610970c3dd61b1194
ale94 <alejandrorua111@gmail.com>
2024-09-24 22:45:32 -0300

    feat: databaseconfig created

---
+++ src/main/java/ar/com/ale94/config/DatabaseConfig.java
@@ -0,0 +1,7 @@
+package ar.com.ale94.config;
+
+public class DatabaseConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc_example";
+    public static final String USER = "root";
+    public static final String PASSWORD = "alejandro12";
+}
