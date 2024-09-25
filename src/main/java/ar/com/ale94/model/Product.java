34df15bb5fb24836febba3b69f889b407f45431c
ale94 <alejandrorua111@gmail.com>
2024-09-24 22:53:13 -0300

    feat: Product entity created

---
+++ src/main/java/ar/com/ale94/model/Product.java
@@ -0,0 +1,67 @@
+package ar.com.ale94.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, Long id, Double price, Integer quantity) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, Double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Product{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append(", quantity=").append(quantity);
+        sb.append('}');
+        return sb.toString();
+    }
+}
